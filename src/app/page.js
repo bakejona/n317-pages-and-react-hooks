6c37e1accf73ce0de930be5b1bc944c395bc25e4
bakejona <jbakerbba2@gmail.com>
2024-09-18 10:12:58 -0400

    restarted application

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,7 @@
-"use cliet";
-
 export default function Home() {
-  let score = 0;
-
-  function addToScore() {
-    score ++;
-  }
   return (
     <main>
-        <h1>Home</h1>
-        <div>
-          <button onClick={addToScore()}>Add 1</button>
-          <p>Score: {score}</p>
-        </div>
+      <h1>Home</h1>
     </main>
-);
+  );
 }
--- src/app/pokemon/page.js
+++ src/app/pokemon/page.js
@@ -1,9 +1,7 @@
-import './globals.css'
-import './page.module.css'
 export default function Pokemon() {
     return (
-        <main>
-            <h1>Pokemon Page</h1>
-        </main>
+      <main>
+        <h1>Pokemon Page</h1>
+      </main>
     );
-}
+  }
