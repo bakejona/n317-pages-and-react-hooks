1fcd2e7ebe7011fd562db28710ab3ba14fb4b8e0
bakejona <jbakerbba2@gmail.com>
2024-09-18 10:29:17 -0400

    Added about page

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,25 @@
+"use client";
+import { useState } from 'react';
+
 export default function Home() {
+  const [score, setScore] = useState(1);
+
+  console.log(score);
+
+  function addToScore() {
+    setScore(score + 1);
+    console.log(score);
+
+  }
   return (
     <main>
       <h1>Home</h1>
+      <div>
+        <button onClick={addToScore}>Add 1</button>
+        <p>Score: {score}</p>
+      </div>
     </main>
   );
 }
+
+
